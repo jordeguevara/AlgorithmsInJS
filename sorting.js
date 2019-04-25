55ff601734ab9f9b0db4e849a74e401687b280d2
Jorde Guevara <jguevara60@toromail.csudh.edu>
2019-04-24 19:28:38 -0700

    added merge func

--- sorting.js
+++ sorting.js
@@ -6,20 +6,49 @@ let actual = (unsortedArr) => {
     return sorted;
 }
 const insertionSort = (arr) =>  {
-    for(let i = 1; i < arr.length;i++){
-        let current = arr[i];
+    const result = JSON.parse(JSON.stringify(arr));
+    for(let i = 1; i < result.length;i++){
+        let current = result[i];
         let j = i -1;
-        while(j >= 0 && arr[j] > current){
-            arr[j + 1] = arr[j]
+        while(j >= 0 && result[j] > current){
+            result[j + 1] = result[j]
             j = j -1;
-            arr[j+1] = current;
+            result[j+1] = current;
         }
     }
-    return arr;
+    return result;
+}
+
+const mergeSort = (arr,low,mid,high) => {
+
+    function merge(){
+        let n1 = mid - low +1;
+        let n2 = high - mid; 
+        let left = [];
+        let right = []
+
+        for(let i = 1 ; i < n1; i++){
+            left.push( arr[low + i -1]);
+        }
+        for(let i = 0 ; i < n2;i++){
+        
+            right.push(arr[mid+i])
+        }
+        return [...left,...right];
+    }
+    return merge;
+
 }
 
 
-console.log(insertionSort(unsortedArr));
+// TO DO: BubbleSort, selctionSort
+
+
+
+const sortedArr = insertionSort(unsortedArr)
+console.log(sortedArr)
+let mid = Math.floor(unsortedArr.length/2);
+console.log(mergeSort(unsortedArr,0,mid,unsortedArr.length)());
 try{
 assert.deepEqual(insertionSort(unsortedArr), actual(unsortedArr));
 }
