60389e19618b860868ef88452c1dbdfd308b4d34
Jorde Guevara <jguevara60@toromail.csudh.edu>
2019-05-04 18:23:13 -0700

    string permutation

---
+++ permutations.js
@@ -0,0 +1,21 @@
+console.log(permuation("abc"));
+function permuation(str) {
+  let res = [];
+  permuations("", str, res);
+  return res;
+}
+
+function permuations(prefix, suffix, arr) {
+  //base case no suffix all in prefix
+  if (suffix.length === 0) {
+    arr.push(prefix);
+  } else {
+    for (let i = 0; i < suffix.length; i++) {
+      permuations(
+        prefix + suffix.charAt(i),
+        suffix.slice(0, i) + suffix.slice(i + 1, suffix.length),
+        arr
+      );
+    }
+  }
+}
--- sorting.js
+++ sorting.js
@@ -1,58 +1,51 @@
-const assert = require('assert');
-let unsortedArr = [ 3,1,5,9,0,-1,2,99, -101];
+const assert = require("assert");
+let unsortedArr = [3, 1, 5, 9, 0, -1, 2, 99, -101];
 
-let actual = (unsortedArr) => {
-    const sorted = unsortedArr.sort((a,b) => a- b)
-    return sorted;
-}
-const insertionSort = (arr) =>  {
-    const result = JSON.parse(JSON.stringify(arr));
-    for(let i = 1; i < result.length;i++){
-        let current = result[i];
-        let j = i -1;
-        while(j >= 0 && result[j] > current){
-            result[j + 1] = result[j]
-            j = j -1;
-            result[j+1] = current;
-        }
+let actual = unsortedArr => {
+  const sorted = unsortedArr.sort((a, b) => a - b);
+  return sorted;
+};
+const insertionSort = arr => {
+  const result = JSON.parse(JSON.stringify(arr));
+  for (let i = 1; i < result.length; i++) {
+    let current = result[i];
+    let j = i - 1;
+    while (j >= 0 && result[j] > current) {
+      result[j + 1] = result[j];
+      j = j - 1;
+      result[j + 1] = current;
     }
-    return result;
-}
+  }
+  return result;
+};
 
-const mergeSort = (arr,low,mid,high) => {
+const mergeSort = (arr, low, mid, high) => {
+  function merge() {
+    let n1 = mid - low + 1;
+    let n2 = high - mid;
+    let left = [];
+    let right = [];
 
-    function merge(){
-        let n1 = mid - low +1;
-        let n2 = high - mid; 
-        let left = [];
-        let right = []
-
-        for(let i = 1 ; i < n1; i++){
-            left.push( arr[low + i -1]);
-        }
-        for(let i = 0 ; i < n2;i++){
-        
-            right.push(arr[mid+i])
-        }
-        return [...left,...right];
+    for (let i = 1; i < n1; i++) {
+      left.push(arr[low + i - 1]);
     }
-    return merge;
+    for (let i = 0; i < n2; i++) {
+      right.push(arr[mid + i]);
+    }
 
-}
-
+    return [...left, ...right];
+  }
+  return merge;
+};
 
 // TO DO: BubbleSort, selctionSort
 
-
-
-const sortedArr = insertionSort(unsortedArr)
-console.log(sortedArr)
-let mid = Math.floor(unsortedArr.length/2);
-console.log(mergeSort(unsortedArr,0,mid,unsortedArr.length)());
-try{
-assert.deepEqual(insertionSort(unsortedArr), actual(unsortedArr));
+const sortedArr = insertionSort(unsortedArr);
+console.log(sortedArr);
+let mid = Math.floor(unsortedArr.length / 2);
+console.log(mergeSort(unsortedArr, 0, mid, unsortedArr.length)());
+try {
+  assert.deepEqual(insertionSort(unsortedArr), actual(unsortedArr));
+} catch (err) {
+  console.log(err);
 }
-catch(err){
-    console.log(err);
-}
- 
