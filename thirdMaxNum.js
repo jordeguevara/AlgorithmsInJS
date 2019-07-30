a37ec61d376269abc1c0318ceab7190c0ec00394
Jorde Guevara <jguevara60@toromail.csudh.edu>
2019-07-29 21:10:14 -0700

    leetcooodee

---
+++ thirdMaxNum.js
@@ -0,0 +1,30 @@
+//414. Third Maximum Number
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    if(nums.length <3)
+        return Math.max(...nums)
+    
+    const sortedDescending = nums.sort((a,b) => b -a)
+    let i = 0
+    let j= 0;
+    let max = Number.NEGATIVE_INFINITY;
+    while(i <3){
+        if(nums[j]=== max){
+            j++;
+            if(j > nums.length)
+                break;
+            continue;
+        }
+          
+        max = sortedDescending[j];
+        i++;
+        j++;
+    }
+
+    let answer = max === undefined ?  Math.max(...nums) : max
+    return answer
+};
+
